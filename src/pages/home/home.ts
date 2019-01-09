d8d6111f0feff3004041a656bb864482731e90d8
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-01-09 10:56:56 -0200

    Projeto criado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AdMobFree, AdMobFreeBannerConfig } from '@ionic-native/admob-free';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private admobFree: AdMobFree) {
 
   }
+
+  showBanner(){
+    const bannerConfig: AdMobFreeBannerConfig = {
+      // add your config here
+      // for the sake of this example we will just use the test config
+      isTesting: true,
+      autoShow: true
+     };
+     
+     this.admobFree.banner.config(bannerConfig);
+  
+  this.admobFree.banner.prepare()
+    .then(() => {
+      // banner Ad is ready
+      // if we set autoShow to false, then we will need to call the show method here
+    })
+    .catch(e => console.log(e));
+  }
+
 
 }
